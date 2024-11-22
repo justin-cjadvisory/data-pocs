71b50885a255e903f99779a40e1d3c97597801c8
jddeguia-sellerx <justin.deguia@sellerx.com>
2024-11-22 12:23:50 +0800

    mart layer pnl

---
+++ cj_data_models/models/marts/accounting/profit_and_loss/mart_accounting__profit_and_loss_table.sql
@@ -0,0 +1,13 @@
+{{
+  config(
+    materialized='table',
+    tags=["module:accounting", "submodule:profit_and_loss"]
+  )
+}}
+
+WITH stg_base AS (
+    SELECT *
+    FROM  {{ ref('stg_accounting__profit_and_loss_table') }}
+)
+
+SELECT * FROM stg_base
