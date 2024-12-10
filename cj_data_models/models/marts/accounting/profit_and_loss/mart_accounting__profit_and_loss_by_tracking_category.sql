9af7426359da4865a091acab9e622c48f1e991f3
justin-cjadvisory <justin@cjadvisory.com.au>
2024-12-10 11:51:25 +0800

    Update mart layer pnl by build (#14)
    * update mart layer pnl by build
    
    * update cte name
    
    ---------
    
    Co-authored-by: jddeguia-sellerx <justin.deguia@sellerx.com>

--- cj_data_models/models/marts/accounting/profit_and_loss/mart_accounting__profit_and_loss_by_tracking_category.sql
+++ cj_data_models/models/marts/accounting/profit_and_loss/mart_accounting__profit_and_loss_by_tracking_category.sql
@@ -8,6 +8,35 @@
 WITH stg_base AS (
     SELECT *
     FROM  {{ ref('stg_accounting__profit_and_loss_by_tracking_category') }}
+),
+
+load_historical_data AS (
+    SELECT *
+    FROM {{ ref('stg_accounting__profit_and_loss_by_tracking_category_historical') }}
+),
+
+populate_data AS (
+    SELECT
+      EXTRACT(YEAR FROM h.from_date) AS year,
+      EXTRACT(MONTH FROM h.from_date) AS month,
+      h.from_date,
+      h.to_date,
+      h.build,
+      h.account_line_type,
+      h.account_name,
+      h.amount
+    FROM load_historical_data h
+    UNION ALL
+    SELECT
+      EXTRACT(YEAR FROM s.from_date) AS year,
+      EXTRACT(MONTH FROM s.from_date) AS month,
+      s.from_date,
+      s.to_date,
+      s.build,
+      s.account_line_type,
+      s.account_name,
+      s.amount
+    FROM stg_base s
 )
 
-SELECT * FROM stg_base
+SELECT * FROM populate_data
