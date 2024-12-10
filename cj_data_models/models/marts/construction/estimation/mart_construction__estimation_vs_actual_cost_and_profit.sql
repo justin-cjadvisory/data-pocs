7f998ecae418dd5cd7e835813c7d9111aac35a84
jddeguia-sellerx <justin.deguia@sellerx.com>
2024-12-10 22:09:54 +0800

    update estimation model in mart layer

--- cj_data_models/models/marts/construction/estimation/mart_construction__estimation_vs_actual_cost_and_profit.sql
+++ cj_data_models/models/marts/construction/estimation/mart_construction__estimation_vs_actual_cost_and_profit.sql
@@ -7,10 +7,6 @@
 
 WITH estimated_pnl_per_build AS (
     SELECT 
-        year,
-        month,
-        from_date,
-        to_date,
         build,
         cost,
         cost_with_markup,
@@ -20,10 +16,6 @@ WITH estimated_pnl_per_build AS (
 
 xero_pnl_per_build AS (
     SELECT 
-        year,
-        month,
-        from_date,
-        to_date,
         build,
         account_line_type,
         amount
@@ -32,26 +24,20 @@ xero_pnl_per_build AS (
 
 actual_profit_and_cost AS (
     SELECT
-        e.year,
-        e.month,
-        e.from_date,
-        e.to_date,
         e.build,
         SUM(e.cost_with_markup) AS estimated_cost_with_markup,
         SUM(e.profit) AS estimated_profit,
         SUM(IF(x.account_line_type IN ('Less Operating Expenses', 'Less Cost of Sales'), amount, 0)) AS actual_cost,
         SUM(IF(x.account_line_type IN ('Income'), amount, 0)) AS actual_income
     FROM estimated_pnl_per_build e
-    LEFT JOIN xero_pnl_per_build x on x.from_date = e.from_date 
-        AND x.to_date = e.to_date
-        AND x.build = e.build
+    LEFT JOIN xero_pnl_per_build x on x.build = e.build 
     GROUP BY ALL
 ),
 
 compute_actual_profit AS (
     SELECT
         ap.*,
-        SUM(actual_income - actual_cost) AS actual_profit
+        SUM(actual_income) - SUM(actual_cost) AS actual_profit
     FROM actual_profit_and_cost ap
     GROUP BY ALL
 ),
@@ -62,13 +48,13 @@ create_flags AS (
         CASE 
             WHEN actual_cost > estimated_cost_with_markup THEN TRUE
             ELSE FALSE
-        END AS exceeded_cost,
+        END AS exceeded_estimated_cost,
         CASE 
             WHEN actual_profit > estimated_profit THEN TRUE
             ELSE FALSE
-        END AS exceeded_profit
+        END AS exceeded_estimated_profit
     FROM compute_actual_profit c
 )
 
 SELECT * FROM create_flags
-WHERE from_date IS NOT NULL
+WHERE (actual_cost > 0 AND actual_income > 0)
