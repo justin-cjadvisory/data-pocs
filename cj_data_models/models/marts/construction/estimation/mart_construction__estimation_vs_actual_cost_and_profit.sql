2a0f1bc33cbf3f81d2c64429c2a05f1dc4cf566b
justin-cjadvisory <justin@cjadvisory.com.au>
2024-12-10 22:12:23 +0800

    Update mart layer for estimation model (#15)
    * initial draft
    
    * add flags on estimation model
    
    * update estimation model in mart layer
    
    * update estimation model in mart layer P2
    
    ---------
    
    Co-authored-by: jddeguia-sellerx <justin.deguia@sellerx.com>

---
+++ cj_data_models/models/marts/construction/estimation/mart_construction__estimation_vs_actual_cost_and_profit.sql
@@ -0,0 +1,59 @@
+{{
+    config(
+        materialized='table',
+        tags=["module:construction", "submodule:estimation"]
+    )
+}}
+
+WITH estimated_pnl_per_build AS (
+    SELECT 
+        build,
+        cost,
+        cost_with_markup,
+        profit
+    FROM {{ ref('mart_construction__estimation_cost_and_profit') }} 
+),
+
+xero_pnl_per_build AS (
+    SELECT 
+        build,
+        account_line_type,
+        amount
+    FROM {{ ref('mart_accounting__profit_and_loss_by_tracking_category') }} 
+),
+
+actual_profit_and_cost AS (
+    SELECT
+        e.build,
+        SUM(e.cost_with_markup) AS estimated_cost_with_markup,
+        SUM(e.profit) AS estimated_profit,
+        SUM(IF(x.account_line_type IN ('Less Operating Expenses', 'Less Cost of Sales'), amount, 0)) AS actual_cost,
+        SUM(IF(x.account_line_type IN ('Income'), amount, 0)) AS actual_income
+    FROM estimated_pnl_per_build e
+    INNER JOIN xero_pnl_per_build x on x.build = e.build 
+    GROUP BY ALL
+),
+
+compute_actual_profit AS (
+    SELECT
+        ap.*,
+        SUM(actual_income) - SUM(actual_cost) AS actual_profit
+    FROM actual_profit_and_cost ap
+    GROUP BY ALL
+),
+
+create_flags AS (
+    SELECT 
+        c.*,
+        CASE 
+            WHEN actual_cost > estimated_cost_with_markup THEN TRUE
+            ELSE FALSE
+        END AS exceeded_estimated_cost,
+        CASE 
+            WHEN actual_profit > estimated_profit THEN TRUE
+            ELSE FALSE
+        END AS exceeded_estimated_profit
+    FROM compute_actual_profit c
+)
+
+SELECT * FROM create_flags
