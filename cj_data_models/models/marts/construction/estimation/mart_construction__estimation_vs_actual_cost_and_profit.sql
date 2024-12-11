19b1b6627ff18c9a2c3b157b875d2b19114d7283
jddeguia-sellerx <justin.deguia@sellerx.com>
2024-12-11 14:19:50 +0800

    quick fix

--- cj_data_models/models/marts/construction/estimation/mart_construction__estimation_vs_actual_cost_and_profit.sql
+++ cj_data_models/models/marts/construction/estimation/mart_construction__estimation_vs_actual_cost_and_profit.sql
@@ -8,36 +8,36 @@
 WITH estimated_pnl_per_build AS (
     SELECT 
         build,
-        cost,
-        cost_with_markup,
-        profit
+        SUM(cost_with_markup) AS cost_with_markup,
+        SUM(profit) AS profit
     FROM {{ ref('mart_construction__estimation_cost_and_profit') }} 
+    GROUP BY ALL
 ),
 
 xero_pnl_per_build AS (
     SELECT 
         build,
-        account_line_type,
-        amount
+        SUM(IF(account_line_type IN ('Less Operating Expenses', 'Less Cost of Sales'), amount, 0)) AS actual_cost,
+        SUM(IF(account_line_type IN ('Income'), amount, 0)) AS actual_income
     FROM {{ ref('mart_accounting__profit_and_loss_by_tracking_category') }} 
+    GROUP BY ALL
 ),
 
 actual_profit_and_cost AS (
     SELECT
         e.build,
-        SUM(e.cost_with_markup) AS estimated_cost_with_markup,
-        SUM(e.profit) AS estimated_profit,
-        SUM(IF(x.account_line_type IN ('Less Operating Expenses', 'Less Cost of Sales'), amount, 0)) AS actual_cost,
-        SUM(IF(x.account_line_type IN ('Income'), amount, 0)) AS actual_income
+        e.cost_with_markup AS estimated_cost_with_markup,
+        e.profit AS estimated_profit,
+        x.actual_cost,
+        x.actual_income
     FROM estimated_pnl_per_build e
-    INNER JOIN xero_pnl_per_build x on x.build = e.build 
-    GROUP BY ALL
+    LEFT JOIN xero_pnl_per_build x on x.build = e.build 
 ),
 
 compute_actual_profit AS (
     SELECT
         ap.*,
-        SUM(actual_income) - SUM(actual_cost) AS actual_profit
+       actual_income - actual_cost AS actual_profit
     FROM actual_profit_and_cost ap
     GROUP BY ALL
 ),
